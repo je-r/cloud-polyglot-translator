6adab64f0049bff5a117e9ae0a3429579752318a
J.E. R <5433476+je-r@users.noreply.github.com>
2018-11-09 08:34:22 +0100

    Update polyglot.js

--- js/polyglot.js
+++ js/polyglot.js
@@ -1,6 +1,6 @@
 /* polyglot.js  */
 
-var jsVersion = 16;
+var jsVersion = 18;
 
 function resetIt()
 {
@@ -42,7 +42,7 @@ function processWebserviceResult(theResponseText, theLabel, targetLang)
 	//	" data-clipboard-target='#theText4"+targetLang+"' >Copy to clipboard</button>";
 	
 	var sourceText = $("input#theSource").val();
-	var sourceLanguage = $('input[type=radio][name=sourceRadio]').val();
+	var sourceLanguage = $('input[type=radio][name=sourceRadio]:checked').val();
 	var copyGUI = "<input class='polyglotButton' "+
 		" data-clipboard-target='#theText4"+targetLang+"' type='button' value='Copy to clipboard' >" +
 	    " <a href='https://translate.google.com/#"+sourceLanguage+"/"+targetLang+"/"+sourceText+"' target='_blank' >Google Translate</a>";
